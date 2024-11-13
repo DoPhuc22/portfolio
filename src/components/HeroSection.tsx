6d53bad709b652f5b174b28ba1f45911ee6f9419
Do Hong Phuc <phucjzaw@gmail.com>
2024-11-13 10:06:25 +0700

    fix hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -33,10 +33,6 @@ const HeroSection = () => {
               repeat={Infinity}
             />
           </h1>
-          <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,
-            voluptuous.
-          </p>
           <div>
             <Link
               href="/#contact"
